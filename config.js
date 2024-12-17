592a96958dd36b803d7bb123f6df3f57bc1d0c81
ucheben10 <111978438+ucheben10@users.noreply.github.com>
2024-12-17 13:19:55 +0100

    Update config.js

--- config.js
+++ config.js
@@ -23,9 +23,9 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['2349013432338', '2349013432338']
-global.prems = ['2349013432338', '2349013432338', '2349013432338']
-global.allowed = ['2349013432338', '2349013432338', '2349013432338']
+global.mods = ['2347065917720', '2347065917720']
+global.prems = ['2347065917720', '2347065917720', '2347065917720']
+global.allowed = ['2347065917720', '2347065917720', '2347065917720']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
